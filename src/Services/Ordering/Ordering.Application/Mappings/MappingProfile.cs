175c232737ebe8c7f1faff7f54d474ecd48f25ef
thananat rueangchot <thananat.dev@gmail.com>
2022-07-13 20:05:36 +0700

    add Orders.API // Orders.Application // Orders.Domain

---
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Ordering.Domian.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //CreateMap<Order, OrdersVm>().ReverseMap();
+            //CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            //CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastucture/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ordering.Infrastucture
+{
+    public class Class1
+    {
+    }
+}
